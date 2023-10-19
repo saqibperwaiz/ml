94d444e7b1c80a86daddb4e426831878e2a3b252
AreebAhmad-02 <105370289+AreebAhmad-02@users.noreply.github.com>
2023-10-19 20:50:31 +0500

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,1 +1,10 @@
+def lambda_handler(event, context):
+    response = {
+        'statusCode': 200,
+        'body': 'This is Hello World #1 and this is Predictor example',
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+    return response
 
